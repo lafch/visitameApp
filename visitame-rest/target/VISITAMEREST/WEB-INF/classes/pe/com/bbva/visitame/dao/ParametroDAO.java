f7c6c6abc8f4037be35553fb722c7795274e3bdf
luis <albeflores@gmail.com>
2017-06-08 16:31:38 -0500

    comentario

--- visitame-rest/target/VISITAMEREST/WEB-INF/classes/pe/com/bbva/visitame/dao/ParametroDAO.java
+++
@@ -1,7 +0,0 @@
-package pe.com.bbva.visitame.dao;
-
-import pe.com.bbva.visitame.dominio.Parametro;
-
-public interface ParametroDAO extends BaseDAO<Parametro, String> {
-
-}
--- visitame-rest/target/m2e-wtp/web-resources/WEB-INF/classes/pe/com/bbva/visitame/dao/ValorDAO.java
+++
@@ -1,7 +0,0 @@
-package pe.com.bbva.visitame.dao;
-
-import pe.com.bbva.visitame.dominio.Valor;
-
-public interface ValorDAO extends BaseDAO<Valor, Long> {
-	
-}
