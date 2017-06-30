0e9cb6dc07cedaec25df21beb73d0689f5cdb42d
Arturo <Arturo@CDA-PE-0076.cdainfo.local>
2017-06-30 10:24:39 -0500

    Registro Persona

--- visitame-rest/src/main/java/pe/com/bbva/visitame/dao/IntentoLogueoDAO.java
+++ visitame-rest/src/main/java/pe/com/bbva/visitame/dao/IntentoLogueoDAO.java
@@ -1,7 +1,12 @@
 package pe.com.bbva.visitame.dao;
 
+import java.util.Date;
+
 import pe.com.bbva.visitame.dominio.IntentoLogueo;
+import pe.com.bbva.visitame.exception.DAOException;
 
 public interface IntentoLogueoDAO extends BaseDAO<IntentoLogueo, String> {
+	
+	Integer contarIntentosPorDia(Integer doi , String numDoc , Date fecha) throws DAOException;
 
 }
